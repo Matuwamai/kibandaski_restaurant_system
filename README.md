197af7a1ab0ac54c7539aff35b67aec965a07310
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-11-24 23:00:10 +0300

    Initialize project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Restaurant System Backend with Django
---
+++ meals_and_dishes/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MealsAndDishesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'meals_and_dishes'
