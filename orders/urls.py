f8cb1be47d3ae8c9012c41b4ed420dad6d794ae0
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-11-25 04:16:52 +0300

    Add view to update order status

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,7 @@ from orders import views
 
 urlpatterns = [
     path('create', views.create_order, name='create_order'),
-    path('list', views.list_orders, name='list_all_orders')
+    path('list', views.list_orders, name='list_all_orders'),
+    path('edit-status/<int:order_id>', views.update_order_status,
+         name='update_order_status')
 ]
--- orders/views.py
+++ orders/views.py
@@ -15,9 +15,29 @@ def create_order(request):
 
     return Response(serializer.errors, status=400)
 
+# Get all orders
+
 
 @api_view(['GET'])
 def list_orders(request):
     orders = Order.objects.all()
     serializer = ListOrdersSerializer(orders, many=True)
+    return Response(serializer.data)
+
+# Update order
+
+
+@api_view(['PUT'])
+def update_order_status(request, order_id):
+    try:
+        order = Order.objects.get(id=order_id)
+    except Order.DoesNotExist:
+        return Response({"message": "Order not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    # Update the is_completed field
+    order.is_completed = True
+    order.save()
+
+    # Serialize the updated order
+    serializer = ListOrdersSerializer(order)
     return Response(serializer.data)
