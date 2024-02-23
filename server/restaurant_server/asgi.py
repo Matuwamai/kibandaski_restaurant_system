530e233906e1b61a78bb00a0c70ba6570ed423ab
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-02-23 19:26:03 +0300

    Setup django setting module

--- server/restaurant_server/asgi.py
+++ server/restaurant_server/asgi.py
@@ -1,16 +1,14 @@
-# isort: skip_file
+import os
+import django
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE', 'restaurant_server.settings')
+django.setup()
 from django.core.asgi import get_asgi_application
 django_asgi_app = get_asgi_application()
-import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
 from channels_app import routing  # Import your routing module
-
-# import django
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant_server.settings')
-# django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
--- server/restaurant_server/wsgi.py
+++ server/restaurant_server/wsgi.py
@@ -1,8 +1,9 @@
 
 import os
-
+import django
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE', 'restaurant_server.settings')
+django.setup()
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant_server.settings')
 
 application = get_wsgi_application()
